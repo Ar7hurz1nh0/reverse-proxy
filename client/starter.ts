ab591f25191a2ad15ec95666db503335095952db
Ar7hurz1nh0 <arthurbr@cdmd.dev>
2023-06-15 12:51:56 -0300

    added colors :)

--- client/starter.ts
+++ client/starter.ts
@@ -1,6 +1,14 @@
-import { readFileSync } from 'fs';
-import { connect, Socket } from 'net';
-import { createHash } from 'crypto';
+import { readFileSync } from 'node:fs';
+import { connect, Socket } from 'node:net';
+import { createHash } from 'node:crypto';
+import chalk from 'chalk';
+
+const c = {
+  info: (level: string, ...message: unknown[]) => console.log(chalk`{blue [${level}]} ${message.join(' ')}`),
+  warn: (level: string, ...message: unknown[]) => console.log(chalk`{yellow [${level}]} ${message.join(' ')}`),
+  error: (level: string, ...message: unknown[]) => console.log(chalk`{red [${level}]} ${message.join(' ')}`),
+  debug: (level: string, ...message: unknown[]) => console.log(chalk`{magenta [${level}]} ${message.join(' ')}`)
+}
 
 const config: {
   targets: {
@@ -16,7 +24,7 @@ const config: {
 } = JSON.parse(readFileSync('config.json', 'utf8'));
 
 function onConnect(s?: Socket): void {
-  console.log("[MAIN]", "Connected to", `${config.redirect_to.address}:${config.redirect_to.port}`);
+  c.info("MAIN", "Connected to", `${config.redirect_to.address}:${config.redirect_to.port}`);
   (s ?? socket).write(config.auth + '\n' + config.targets.map(t => t.port).join(' '))
 }
 
@@ -30,27 +38,27 @@ function appendHeader(data: Buffer, id: string): Buffer {
 
 const connections: Map<string, Socket> = new Map();
 
-console.log("[MAIN]", "Attempting to connect to", `${config.redirect_to.address}:${config.redirect_to.port}`)
+c.info("MAIN", "Attempting to connect to", `${config.redirect_to.address}:${config.redirect_to.port}`)
 const socket = connect(config.redirect_to.port, config.redirect_to.address, onConnect)
 
 socket.on('error', data => {
-  console.log("[MAIN]", "Error while connecting to target:", data.message ?? data)
+  c.error("MAIN", "Error while connecting to target:", data.message ?? data)
   setTimeout(() => {
-    console.log("[MAIN]", "Attempting to reconnect to target")
+    c.info("MAIN", "Attempting to reconnect to target")
     socket.connect(config.redirect_to.port, config.redirect_to.address)
   }, 5000)
 })
 
 socket.on('close', error => {
   if (error) return;
-  console.log("[MAIN]", "Server closed connection, finishing it up")
+  c.warn("MAIN", "Server closed connection, finishing it up")
   socket.end();
 })
 
 socket.on('data', data => {
   const [header] = data.toString('binary').split(config.separator, 1);
   if (typeof header === 'undefined') {
-    console.log("[MAIN]", "Invalid header, ignoring")
+    c.warn("MAIN", "Invalid header, ignoring")
     return;
   }
   const body = data.subarray(header.length + config.separator.length);
@@ -58,85 +66,85 @@ socket.on('data', data => {
   const body_str = body.toString('binary')
   const sha1 = createHash('sha1').update(body_str).digest('hex');
   const sha512 = createHash('sha512').update(body_str).digest('hex');
-  console.log(`[CONN_${port_str}/${id}]`, "Expected:", sha1_dig)
-  console.log(`[CONN_${port_str}/${id}]`, "Got:     ", sha1)
-  console.log(`[CONN_${port_str}/${id}]`, "Expected:", sha512_dig)
-  console.log(`[CONN_${port_str}/${id}]`, "Got:     ", sha512)
+  c.info(`CONN_${port_str}/${id}`, "Expected:", sha1_dig)
+  c.info(`CONN_${port_str}/${id}`, "Got:     ", sha1)
+  c.info(`CONN_${port_str}/${id}`, "Expected:", sha512_dig)
+  c.info(`CONN_${port_str}/${id}`, "Got:     ", sha512)
   if (sha1 !== sha1_dig || sha512 !== sha512_dig) {
-    console.log("[MAIN]", "Invalid checksum, ignoring")
+    c.error("MAIN", "Invalid checksum, ignoring")
     return;
-  } else console.log("[MAIN]", "Checksums match")
+  } else c.info("MAIN", "Checksums match")
   if (typeof id === 'undefined' || typeof port_str === 'undefined') {
-    console.log("[MAIN]", "Invalid packet, ignoring")
+    c.error("MAIN", "Invalid packet, ignoring")
     if (typeof id === 'undefined')
-      console.log("[MAIN]", "Invalid id, ignoring")
+      c.error("MAIN", "Invalid id, ignoring")
     if (typeof port_str === 'undefined')
-      console.log("[MAIN]", "Invalid port, ignoring")
+      c.error("MAIN", "Invalid port, ignoring")
     return;
   }
   const port = parseInt(port_str);
-  console.log(`[CONN_${port}/${id}]`, "Received data from connection")
+  c.info(`CONN_${port}/${id}`, "Received data from connection")
   if (!connections.has(id)) {
     const conn_socket = connect(port, config.targets.find(t => t.port === port)?.address ?? 'localhost', () => {
-      console.log(`[CONN_${port}/${id}]`, `Connected to target`)
+      c.info(`CONN_${port}/${id}`, `Connected to target`)
     })
     connections.set(id, conn_socket)
 
     conn_socket.on('data', data => {
-      console.log(`[CONN_${port}/${id}]`, "Received data from target")
+      c.info(`CONN_${port}/${id}`, "Received data from target")
       socket.write(appendHeader(data, id))
     })
 
     conn_socket.on('close', () => {
-      console.log(`[CONN_${port}/${id}]`, "Closing connection")
+      c.warn(`CONN_${port}/${id}`, "Closing connection")
       connections.delete(id)
     })
 
     conn_socket.on('error', data => {
-      console.log(`[CONN_${port}/${id}]`, data.message ?? data)
+      c.error(`CONN_${port}/${id}`, data.message ?? data)
     })
 
     conn_socket.on('end', () => {
       conn_socket.destroy();
-      console.log(`[CONN_${port}/${id}]`, "Connection destroyed")
+      c.warn(`CONN_${port}/${id}`, "Connection destroyed")
     })
 
     conn_socket.on('timeout', () => {
-      console.log(`[CONN_${port}/${id}]`, "Target timed out, ending connection")
+      c.warn(`CONN_${port}/${id}`, "Target timed out, ending connection")
       conn_socket.end();
     })
 
     conn_socket.on('drain', () => {
-      console.log(`[CONN_${port}/${id}]`, "Target drained buffer")
+      c.warn(`CONN_${port}/${id}`, "Target drained buffer")
     })
 
     conn_socket.on('ready', () => {
-      console.log(`[CONN_${port}/${id}]`, "Target ready")
+      c.info(`CONN_${port}/${id}`, "Target ready")
     })
   }
   const success = connections.get(id)?.write(body)
-  if (success) console.log(`[CONN_${port}/${id}]`, "Successfully flushed buffer to target")
-  else console.log(`[CONN_${port}/${id}]`, "Failed to flush buffer to target:", success)
+  if (success) c.info(`CONN_${port}/${id}`, "Successfully flushed buffer to target")
+  else c.error(`CONN_${port}/${id}`, "Failed to flush buffer to target:", success)
 })
 
 socket.on('end', () => {
   socket.destroy();
-  console.log("[MAIN]", "Server destroyed")
+  c.warn("MAIN", "Server destroyed")
 })
 
 socket.on('timeout', () => {
-  console.log("[MAIN]", "Server timed out, ending connection")
+  c.warn("MAIN", "Server timed out, ending connection")
   socket.end();
 })
 
 socket.on('drain', () => {
-  console.log("[MAIN]", "Server drained buffer")
+  c.warn("MAIN", "Server drained buffer")
 })
 
 socket.on('ready', () => {
-  console.log("[MAIN]", "Server ready")
+  c.info("MAIN", "Server ready")
 })
 
 socket.on('error', data => {
-  console.log("[MAIN]", data.message ?? data)
+  c.error("MAIN", data.message ?? data)
 })
